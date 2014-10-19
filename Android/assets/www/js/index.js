5bf153b91b87107939d8444fec9bedc191d826aa
Ljudevit <ljudevit.habjanec@instantcom.net>
2014-10-20 01:58:15 +0200

    Added Pdf417 Android Plugin to demo project

--- Android/assets/www/js/index.js
+++ Android/assets/www/js/index.js
@@ -16,6 +16,16 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+// implement your decoding as you need it, this just does ASCII decoding
+function hex2a(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    }
+    return str;
+}
+ 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -32,7 +42,83 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function() {        
+
+        var resultDiv = document.getElementById('resultDiv');
+
+        /**
+        * Simple scan example
+        **/
+        scanButton.addEventListener('click', function() {    
+            window.plugins.pdf417Scanner.scan(
+                // Register the callback handler
+                function callback(data) {
+                    //alert("got result " + data.data + " type " + data.type);
+                    if (data.cancelled == true) {
+                        resultDiv.innerHTML = "Cancelled!";
+                    } else if (data.resultList && data.resultList.length > 1) {
+                        // More than 1 element in results
+                        resultDiv.innerHTML = data.resultList.length + " results";
+                    } else {
+                        resultDiv.innerHTML = "Data: " + data.data + " (raw: " + hex2a(data.raw) + ") (Type: " + data.type + ")";
+                    }
+                },
+                // Register the errorHandler
+                function errorHandler(err) {
+                    alert('Error');
+                },
+                [ ["PDF417", "QR Code"], false ] //We want qr codes and pdf417 scanned with the beep sound off
+            );
+        });
+        
+        /**
+        * Scan these barcode types
+        * Available: "PDF417", "QR Code", "Code 128", "Code 39", "EAN 13", "EAN 8", "ITF", "UPCA", "UPCE"
+        **/
+        var types = ["PDF417", "QR Code"];
+
+        /**
+        * Initiate scan with options
+        * NOTE: Some features are unavailable without a license
+        * Obtain your key at http://pdf417.mobi
+        **/
+        var options = {
+            beep : true,
+            noDialog : true,
+            removeOverlay :true,
+            uncertain : false, //Recommended
+            quietZone : false, //Recommended
+            highRes : false, //Recommended
+            frontFace : false
+        };
+
+        // Note that each platform requires its own license key
+
+        // This license key allows setting overlay views for this application ID: net.photopay.barcode.pdf417-sample
+        var licenseiOs = "YHCP-25S4-ZFR3-3LQR-ANPY-MZFZ-VVHO-YPUW";
+
+        // This license is only valid for package name "mobi.pdf417"
+        var licenseAndroid = "FFMV-RULX-VNIU-CSJW-DAC6-JRBU-AKOC-ZV3G";       
+
+        scanWithOptionsButton.addEventListener('click', function() {    
+            window.plugins.pdf417Scanner.scanWithOptions(
+                // Register the callback handler
+                function callback(data) {
+                    //alert("got result " + data.data + " type " + data.type);
+                    if (data.cancelled == true) {
+                        resultDiv.innerHTML = "Cancelled!";
+                    } else {
+                        resultDiv.innerHTML = "Data: " + data.data + " (raw: " + hex2a(data.raw) + ") (Type: " + data.type + ")";
+                    }
+                },
+                // Register the errorHandler
+                function errorHandler(err) {
+                    alert('Error');
+                },
+                types, options, licenseiOs, licenseAndroid
+            );
+        });
+
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
